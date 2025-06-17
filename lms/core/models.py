c80ba1209791b55947f6f2132e921f2bb4a624d0
Badal Thapa <KhBadalYa@gmail.com>
2025-06-17 15:16:58 +0545

    made changes to LMS added templates and bootstrap

--- lms/core/models.py
+++ lms/core/models.py
@@ -1,3 +1,101 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
+SUBJECT = [
+    {'BCT081', 'Software Engineering'},
+    {'BCT082', 'Software Analysis & Desgin'},
+    {'BCT083', 'Operating System'},
+    {'BCT084', 'Object Oriented Programming'},
+    {'BCT085', 'Electronics & Instrumentation'},
+]
+class Student(models.Model):
+    SEMESTER = [
+        ('SEM_ONE', 'Semester One'),
+        ('SEM_TWO', 'Semester Two'),
+        ('SEM_THREE', 'Semester Three'),
+        ('SEM_FOUR', 'Semester Four'),
+        ('SEM_FIVE', 'Semester Five'),
+        ('SEM_SIX', 'Semester Six'),
+        ('SEM_SEVEN', 'Semester Seven'),
+        ('SEM_EIGHT', 'Semester Eight')
+    ]
+    full_name = models.CharField(max_length=200, null=False, blank=False, verbose_name="Student Full Name")
+    email = models.CharField(max_length=100, unique=True, null=False, blank=False, verbose_name="Student Email")
+    semester = models.CharField(max_length=20, choices=SEMESTER, default='N/A', null=True, blank=True)
+    phone_no = models.IntegerField(null=False, blank=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "student"
+        verbose_name_plural = "students"
+        ordering = ['-full_name']
+    
+    def __str__(self):
+        return self.full_name
+
+class Teacher(models.Model):
+    DEPARTMENT = [
+        ('BCA', 'Bachelor of Computer Application'),
+        ('BCT', 'Bachelor of Computer Engineering'),
+        ('BEI', 'Bachelor of Electronics and Information'),
+        ('BCE', 'Bachelor of Civil Engineering')
+    ]
+    full_name = models.CharField(max_length=200, null=False, blank=False, verbose_name="Teacher Full Name")
+    email = models.CharField(max_length=100, unique=True, blank=False, null=True, verbose_name="Teacher Email")
+    department = models.CharField(max_length=5, choices=DEPARTMENT, default='N/A', null=True, blank=True)
+    phone_no = models.IntegerField(null=False, blank=False)
+    join_date = models.DateField(default='Join Date')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'teacher'
+        verbose_name_plural = 'teachers'
+        ordering = ['-full_name']
+    
+    def __str__(self):
+        return self.full_name
+    
+class Assignment(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False, verbose_name='Assignment Title')
+    start_date = models.DateField(default='Start Date', null=False, blank=False, verbose_name='Start Date')
+    end_date = models.DateField(default='End Date', blank=False, null=False, verbose_name='End Date')
+    question_file = models.FileField(upload_to='assignments/questions/', null=True, blank=True, verbose_name='Select Assignment File')
+    question = models.TextField(null=True, blank=True, verbose_name='Assignment Question')
+    remark = models.CharField(max_length=100, null=False, blank=False, verbose_name='Assignment Details')
+    full_mark = models.FloatField(blank=False, null=False)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, verbose_name='Uploaded By')
+    subject = models.CharField(max_length=40, choices=SUBJECT, verbose_name='Subject')
+
+    class Meta:
+        verbose_name = 'assignment'
+        verbose_name_plural = 'assignments'
+        ordering = ['-title']
+    
+    def __str__(self):
+        return self.title
+    
+class Materials(models.Model):
+    MATERIAL_CATEGORY = [
+        {'SLIDE', 'Chapter Slide'},
+        {'TEXT_BOOK', 'A text book'},
+        {'REFERENCE_BOOK', 'A reference book'},
+        {'OLD_QUESTION', 'Previous board exam question'},
+        {'AUDIO_BOOK', 'An audio book'},
+
+    ]
+    title = models.CharField(max_length=100, null=False, blank=False, verbose_name='Material Title')
+    category = models.CharField(max_length=40, choices=MATERIAL_CATEGORY, null=False, blank=False, verbose_name='Category')
+    material_description = models.CharField(max_length=255, default='Description', null=False, blank=False, verbose_name='Material_Description')
+    material_subject = models.CharField(max_length=40, choices=SUBJECT, default='N/A', verbose_name='Material_Subject')
+    material_file = models.FileField(upload_to='material/', null=False, blank=False, verbose_name='Select File')
+
+    class Meta:
+        verbose_name = 'Materials'
+        verbose_name_plural = 'Materials'
+        ordering = ['-title']
+
+    def __str__(self):
+        return self.title
+   
--- lms/core/views.py
+++ lms/core/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, CreateView
+from core.models import Teacher, Student, Assignment, Materials
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html')
+# def home(request):
+#     return render(request, 'home.html')
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class TeacherListView(ListView):
+    model = Teacher
+    template_name = 'teachers/teachers_index.html'
+    context_object_name = 'teachers'
+    paginate_by = 5
+
+class TeacherCreateView(CreateView):
+    model = Teacher
+    fields = ['full_name', 'email', 'dpartment', 'phone_no', 'join_date', 'user']
+    template_name = 'teachers/teachers_form.html'
+    success_url = 'teacher_index'
+
+
+
